89e2a8912447895766a0fcaaf66085b77497a940
Alex Holliday <ajhollid@gmail.com>
2025-03-26 15:27:59 -0700

    add invite hooks

---
+++ src/Hooks/inviteHooks.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { networkService } from "../main";
+const CLIENT_HOST = import.meta.env.VITE_CLIENT_HOST;
+
+const useGetInviteToken = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(undefined);
+	const [token, setToken] = useState(undefined);
+
+	const clearToken = () => {
+		setToken(undefined);
+	};
+
+	const getInviteToken = async ({ email, role }) => {
+		try {
+			const response = await networkService.requestInvitationToken({
+				email,
+				role,
+			});
+			const token = response?.data?.data?.token;
+			if (typeof token === "undefined") {
+				throw new Error("No token");
+			}
+
+			let inviteLink = token;
+
+			if (typeof CLIENT_HOST !== "undefined") {
+				inviteLink = `${CLIENT_HOST}/register/${token}`;
+			}
+
+			setToken(inviteLink);
+		} catch (error) {
+			setError(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return [getInviteToken, clearToken, isLoading, error, token];
+};
+
+export { useGetInviteToken };
