6421080a95a0911f89c7d18d76b5a54569680cea
Vishnu Sreekumaran Nair <200557136@student.georgianc.on.ca>
2025-04-10 14:48:10 -0400

    create new Tablistwrapper component

---
+++ src/Components/Tab/index.jsx
@@ -0,0 +1,28 @@
+import { Box, useTheme } from "@mui/material";
+import { TabList } from "@mui/lab";
+import PropTypes from "prop-types";
+
+const TabListWrapper = ({ value, onChange, children, ...props }) => {
+	const theme = useTheme();
+
+	return (
+		<Box
+			sx={{
+				borderBottom: `1px solid ${theme.palette.primary.lowContrast}`,
+				"& .MuiTabs-root": { height: "fit-content", minHeight: "0" },
+			}}
+		>
+			<TabList value={value} onChange={onChange} {...props}>
+				{children}
+			</TabList>
+		</Box>
+	);
+};
+
+TabListWrapper.propTypes = {
+	value: PropTypes.string,
+	onChange: PropTypes.func,
+	children: PropTypes.node,
+};
+
+export default TabListWrapper;
