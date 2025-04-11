18aea15305212f9f3a5a88882017d13dcca9df1e
Vishnu Sreekumaran Nair <200557136@student.georgianc.on.ca>
2025-04-11 17:17:35 -0400

    update component layout to match imageField

--- src/Components/Inputs/ImageUpload/index.jsx
+++ src/Components/Inputs/ImageUpload/index.jsx
@@ -2,6 +2,8 @@
 import { Button, Box, Stack, Typography } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import Image from "../../../Components/Image";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
 
 // Utils
 import PropTypes from "prop-types";
@@ -49,117 +51,142 @@ const ImageUpload = ({
 	);
 
 	if (src) {
-		return (
-			<Stack alignItems="center">
-				<Image
-					alt="company logo"
-					maxWidth="250px"
-					maxHeight="250px"
-					src={src}
-					sx={{ ...roundStyle }}
-				/>
-			</Stack>
-		);
-	}
-
+        return (
+            <Stack direction="row" justifyContent="center">
+            <Box
+                sx={{
+                width: "250px",
+                height: "250px",
+                overflow: "hidden",
+                backgroundImage: `url(${src})`,
+                backgroundSize: "cover",
+                ...roundStyle,
+                }}
+            />
+            </Stack>
+        );
+    }
+      
 	return (
-		<>
-			<Box
-				minHeight={175}
-				sx={{
-					position: "relative",
-					border: "dashed",
-					borderRadius: theme.shape.borderRadius,
-					borderColor: theme.palette.primary.lowContrast,
-					borderWidth: "2px",
-					transition: "0.2s",
-					"&:hover": {
-						borderColor: theme.palette.primary.main,
-						backgroundColor: "hsl(215, 87%, 51%, 0.05)",
-					},
-				}}
-				onDragLeave={(e) => {
-					e.preventDefault();
-				}}
-				onDragOver={(e) => {
-					e.preventDefault();
-				}}
-				onDrop={(e) => {
-					e.preventDefault();
-					handleImageChange(e?.dataTransfer?.files?.[0]);
-				}}
-			>
-				<Button
-					fullWidth
-					component="label"
-					role={undefined}
-					sx={{
-						height: "100%",
-					}}
-				>
-					<Stack alignItems="center">
-						<CloudUploadIcon />
-						<Typography
-							component="h2"
-							color={theme.palette.primary.contrastTextTertiary}
-						>
-							<Typography
-								component="span"
-								fontSize="inherit"
-								color="info"
-								fontWeight={500}
-							>
-								Click to upload
-							</Typography>{" "}
-							or drag and drop
-						</Typography>
-						<Typography
-							component="p"
-							color={theme.palette.primary.contrastTextTertiary}
-							sx={{ opacity: 0.6 }}
-						>
-							(maximum size: {formatBytes(maxSize)})
-						</Typography>
-					</Stack>
-					<input
-						style={{
-							clip: "rect(0 0 0 0)",
-							clipPath: "inset(50%)",
-							height: 1,
-							overflow: "hidden",
-							position: "absolute",
-							bottom: 0,
-							left: 0,
-							whiteSpace: "nowrap",
-							width: 1,
-						}}
-						onChange={(e) => handleImageChange(e?.target?.files?.[0])}
-						type="file"
-						accept={accept.map((format) => `image/${format}`).join(", ")}
-					/>
-				</Button>
-			</Box>
-			<Typography
-				component="p"
-				sx={{ opacity: 0.6 }}
-			>
-				Supported formats: {accept.join(", ").toUpperCase()}
-			</Typography>
-			{error && (
-				<Typography
-					component="span"
-					className="input-error"
-					color={theme.palette.error.main}
-					mt={theme.spacing(2)}
-					sx={{
-						opacity: 0.8,
-					}}
-				>
-					{error}
-				</Typography>
-			)}
-		</>
-	);
+        <>
+          {src ? (
+            <Stack direction="row" justifyContent="center">
+              <Box
+                sx={{
+                  width: "250px",
+                  height: "250px",
+                  overflow: "hidden",
+                  backgroundImage: `url(${src})`,
+                  backgroundSize: "cover",
+                  ...roundStyle,
+                }}
+              />
+            </Stack>
+          ) : (
+            <>
+              <Box
+                mt={theme.spacing(8)}
+                sx={{
+                  position: "relative",
+                  height: "fit-content",
+                  border: "dashed",
+                  borderRadius: theme.shape.borderRadius,
+                  borderColor: theme.palette.primary.lowContrast,
+                  borderWidth: "2px",
+                  transition: "0.2s",
+                  "&:hover": {
+                    borderColor: theme.palette.primary.main,
+                    backgroundColor: "hsl(215, 87%, 51%, 0.05)",
+                  },
+                }}
+              >
+                <TextField
+                  type="file"
+                  onChange={(e) => handleImageChange(e?.target?.files?.[0])}
+                  sx={{
+                    width: "100%",
+                    "& .MuiInputBase-input[type='file']": {
+                      opacity: 0,
+                      cursor: "pointer",
+                      maxWidth: "500px",
+                      minHeight: "175px",
+                      zIndex: 1,
+                    },
+                    "& fieldset": {
+                      padding: 0,
+                      border: "none",
+                    },
+                  }}
+                />
+                <Stack
+                  alignItems="center"
+                  gap="4px"
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    zIndex: 0,
+                    width: "100%",
+                  }}
+                >
+                  <IconButton
+                    sx={{
+                      pointerEvents: "none",
+                      borderRadius: theme.shape.borderRadius,
+                      border: `solid ${theme.shape.borderThick}px ${theme.palette.primary.lowContrast}`,
+                      boxShadow: theme.shape.boxShadow,
+                    }}
+                  >
+                    <CloudUploadIcon />
+                  </IconButton>
+                  <Typography
+                    component="h2"
+                    color={theme.palette.primary.contrastTextTertiary}
+                  >
+                    <Typography
+                      component="span"
+                      fontSize="inherit"
+                      color="info"
+                      fontWeight={500}
+                    >
+                      Click to upload
+                    </Typography>{" "}
+                    or drag and drop
+                  </Typography>
+                  <Typography
+                    component="p"
+                    color={theme.palette.primary.contrastTextTertiary}
+                    sx={{ opacity: 0.6 }}
+                  >
+                    (maximum size: {formatBytes(maxSize)})
+                  </Typography>
+                </Stack>
+              </Box>
+              <Typography
+                component="p"
+                color={theme.palette.primary.contrastTextTertiary}
+                sx={{ opacity: 0.6 }}
+              >
+                Supported formats: {accept.join(", ").toUpperCase()}
+              </Typography>
+              {error && (
+                <Typography
+                  component="span"
+                  className="input-error"
+                  color={theme.palette.error.main}
+                  mt={theme.spacing(2)}
+                  sx={{
+                    opacity: 0.8,
+                  }}
+                >
+                  {error}
+                </Typography>
+              )}
+            </>
+          )}
+        </>
+    );    
 };
 
 ImageUpload.propTypes = {
