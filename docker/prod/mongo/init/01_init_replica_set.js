fb505278bdeedfb7ff9fc7973bbf4b2809f99d44
Alex Holliday <ajhollid@gmail.com>
2025-04-25 15:07:44 -0700

    wait for primary

--- docker/prod/mongo/init/01_init_replica_set.js
+++ docker/prod/mongo/init/01_init_replica_set.js
@@ -1,15 +1,16 @@
-try {
-  const status = rs.status();
-  printjson(status);
-} catch (e) {
-  if (e.codeName === "NotYetInitialized") {
-    print("Replica set not initialized. Initiating...");
-    rs.initiate({
-      _id: "rs0",
-      members: [{ _id: 0, host: "mongodb:27017" }],
-    });
-  } else {
-    print("Unexpected error during rs.status():");
-    printjson(e);
+function waitForPrimary() {
+  while (true) {
+    try {
+      rs.initiate({
+        _id: "rs0",
+        members: [{ _id: 0, host: "localhost:27017" }],
+      });
+      break;
+    } catch (e) {
+      print("Replica set initiation failed, retrying in 2s...");
+      sleep(2000);
+    }
   }
 }
+
+waitForPrimary();
