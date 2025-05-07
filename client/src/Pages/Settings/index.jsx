e240314c0464aa1a3d9be40f0c8d7b7dfa5fa140
Thirukumaran-T <ThirukumaranT.dev@gmail.com>
2025-05-07 11:24:58 +0530

    CHORE: added some styles to make them reduced height

--- client/src/Pages/Settings/index.jsx
+++ client/src/Pages/Settings/index.jsx
@@ -190,6 +190,7 @@ const Settings = () => {
 	const languages = Object.keys(i18n.options.resources || {});
 
 	return (
+		<>
 		<Box
 			className="settings"
 			style={{
@@ -411,37 +412,39 @@ const Settings = () => {
 						/>
 					</Box>
 				</ConfigBox>
-				<Box
-				  	sx={{
-						position: "sticky",
-						bottom: 0,
-						background: "rgba(255, 255, 255, 0.9)",
-						boxShadow: "0 -4px 12px rgba(0,0,0,0.5)",
-						backdropFilter: "blur(4px)",
-						zIndex: 1000,
-						pt: theme.spacing(4),
-						pb: theme.spacing(4),
-						px: theme.spacing(8),
-					}}
-				>
-					<Stack
-						direction="row"
-						justifyContent="flex-end"
-					>
-						<Button
-							loading={isLoading || authIsLoading || checksIsLoading}
-							disabled={Object.keys(errors).length > 0}
-							variant="contained"
-							color="accent"
-							sx={{ px: theme.spacing(12), mt: theme.spacing(20) }}
-							onClick={handleSave}
-						>
-							{t("settingsSave")}
-						</Button>
-					</Stack>
-				</Box>
 			</Stack>
 		</Box>
+		<Box
+		sx={{
+			position: "sticky",
+			bottom: 0,
+			background: "rgba(255, 255, 255, 0.9)",
+			boxShadow: "0 -4px 12px rgba(0,0,0,0.1)",
+			backdropFilter: "blur(4px)",
+			zIndex: 1000,
+			mt : 3
+		}}
+			  >
+				  <Stack
+					  direction="row"
+					  justifyContent="flex-end"
+					  sx={{ pr: 15, pl: 5, pt: 5, pb: 5 }}
+				  >
+					  <Button
+						  loading={isLoading || authIsLoading || checksIsLoading}
+						  disabled={Object.keys(errors).length > 0}
+						  variant="contained"
+						  color="accent"
+						  sx={{ px: theme.spacing(12),
+								height: '100%'
+						   }}
+						  onClick={handleSave}
+					  >
+						  {t("settingsSave")}
+					  </Button>
+				  </Stack>
+		  </Box>
+		  </>
 	);
 };
 
