e8d636dcfdfaaafc2ac3e48343c797d6544e1884
Asim Ahmed Siddiqui <asimahmed.s@somaiya.edu>
2025-05-23 12:40:47 +0530

    Fix: Optionally show IP/URL of the monitor in status page

--- client/src/Pages/StatusPage/Status/Components/MonitorsList/index.jsx
+++ client/src/Pages/StatusPage/Status/Components/MonitorsList/index.jsx
@@ -8,9 +8,16 @@ import { StatusLabel } from "../../../../../Components/Label";
 import { useTheme } from "@mui/material/styles";
 import useUtils from "../../../../Uptime/Monitors/Hooks/useUtils";
 import PropTypes from "prop-types";
+
+import { useSelector } from "react-redux";
+
 const MonitorsList = ({ isLoading = false, shouldRender = true, monitors = [] }) => {
 	const theme = useTheme();
 	const { determineState } = useUtils();
+
+	const { statusURL } = useSelector((state) => state.ui);
+	const showURL = statusURL === "enabled";
+
 	return (
 		<>
 			{monitors?.map((monitor) => {
@@ -27,6 +34,7 @@ const MonitorsList = ({ isLoading = false, shouldRender = true, monitors = [] })
 							title={monitor.name}
 							percentageColor={monitor.percentageColor}
 							percentage={monitor.percentage}
+							showURL={showURL}
 						/>
 						<Stack
 							direction="row"
--- server/validation/joi.js
+++ server/validation/joi.js
@@ -427,6 +427,7 @@ const updateAppSettingsBodyValidation = joi.object({
 	checkTTL: joi.number().allow(""),
 	pagespeedApiKey: joi.string().allow(""),
 	language: joi.string().allow(""),
+	statusURL: joi.string().required(),
 	systemEmailHost: joi.string().allow(""),
 	systemEmailPort: joi.number().allow(""),
 	systemEmailAddress: joi.string().allow(""),
