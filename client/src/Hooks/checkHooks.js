289aa4fe32dd44d383898b5c200054e70df8762b
Alex Holliday <ajhollid@gmail.com>
2025-06-12 15:47:21 +0800

    refactor check hooks

--- client/src/Hooks/checkHooks.js
+++ client/src/Hooks/checkHooks.js
@@ -1,32 +1,60 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { networkService } from "../main";
 import { createToast } from "../Utils/toastUtils";
 
-const useFetchChecks = ({ monitorId, monitorType, dateRange, page, rowsPerPage }) => {
+const useFetchChecks = ({
+	teamId,
+	monitorId,
+	type,
+	status,
+	sortOrder,
+	limit,
+	dateRange,
+	filter,
+	page,
+	rowsPerPage,
+}) => {
 	const [checks, setChecks] = useState(undefined);
 	const [checksCount, setChecksCount] = useState(undefined);
 	const [isLoading, setIsLoading] = useState(false);
 	const [networkError, setNetworkError] = useState(false);
 
 	useEffect(() => {
-		if (!monitorType) {
-			return;
-		}
-
 		const fetchChecks = async () => {
+			if (!type && !teamId) {
+				return;
+			}
+
+			const method = monitorId
+				? networkService.getChecksByMonitor
+				: networkService.getChecksByTeam;
+
+			const config = monitorId
+				? {
+						monitorId,
+						type,
+						status,
+						sortOrder,
+						limit,
+						dateRange,
+						filter,
+						page,
+						rowsPerPage,
+					}
+				: {
+						status,
+						teamId,
+						sortOrder,
+						limit,
+						dateRange,
+						filter,
+						page,
+						rowsPerPage,
+					};
+
 			try {
 				setIsLoading(true);
-				const res = await networkService.getChecksByMonitor({
-					monitorId: monitorId,
-					type: monitorType,
-					sortOrder: "desc",
-					limit: null,
-					dateRange: dateRange,
-					filter: null,
-					page: page,
-					rowsPerPage: rowsPerPage,
-				});
+				const res = await method(config);
 				setChecks(res.data.data.checks);
 				setChecksCount(res.data.data.checksCount);
 			} catch (error) {
@@ -36,8 +64,20 @@ const useFetchChecks = ({ monitorId, monitorType, dateRange, page, rowsPerPage }
 				setIsLoading(false);
 			}
 		};
+
 		fetchChecks();
-	}, [monitorId, monitorType, dateRange, page, rowsPerPage]);
+	}, [
+		monitorId,
+		teamId,
+		type,
+		status,
+		sortOrder,
+		limit,
+		dateRange,
+		filter,
+		page,
+		rowsPerPage,
+	]);
 
 	return [checks, checksCount, isLoading, networkError];
 };
