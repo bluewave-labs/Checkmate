bea23a94bb6a042faaea8ee3b6ce1b883f5a17ce
Alex Holliday <ajhollid@gmail.com>
2025-06-16 10:38:05 +0800

    handle discord webhooks

--- client/src/Pages/Notifications/utils.js
+++ client/src/Pages/Notifications/utils.js
@@ -1,6 +1,7 @@
 export const NOTIFICATION_TYPES = [
 	{ _id: 1, name: "E-mail", value: "email" },
-	{ _id: 2, name: "Slack", value: "webhook" },
+	{ _id: 2, name: "Slack", value: "slack" },
 	{ _id: 3, name: "PagerDuty", value: "pager_duty" },
 	{ _id: 4, name: "Webhook", value: "webhook" },
+	{ _id: 5, name: "Discord", value: "discord" },
 ];
