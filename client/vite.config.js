4f17e8424762bd354204dc48decf9fd27bd016b2
Alex Holliday <ajhollid@gmail.com>
2025-06-24 15:40:50 +0800

    extract version from git tag

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { execSync } from "child_process";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	base: "/",
-	plugins: [svgr(), react()],
-	optimizeDeps: {
-		include: ["@mui/material/Tooltip", "@emotion/styled"],
-	},
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	const version =
+		env.VITE_APP_VERSION || execSync("git describe --tags --abbrev=0").toString().trim();
+	return {
+		base: "/",
+		plugins: [svgr(), react()],
+		optimizeDeps: {
+			include: ["@mui/material/Tooltip", "@emotion/styled"],
+		},
+		define: {
+			__APP_VERSION__: JSON.stringify(version),
+		},
+	};
 });
