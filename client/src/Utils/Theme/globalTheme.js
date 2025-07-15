5a2626b38b15dc103dc7c5ff1eef8f1c3aecd610
karenvicent <karenvergara@msn.com>
2025-07-15 14:39:41 -0400

    feat: apply styleOverrides for MuiSelect in base theme

--- client/src/Utils/Theme/globalTheme.js
+++ client/src/Utils/Theme/globalTheme.js
@@ -553,6 +553,13 @@ const baseTheme = (palette) => ({
 					"&:hover .MuiOutlinedInput-notchedOutline": {
 						borderColor: theme.palette.primary.lowContrast,
 					},
+					"& .MuiSelect-select": {
+						padding: "0 13px",
+						minHeight: "34px",
+						display: "flex",
+						alignItems: "center",
+						lineHeight: 1,
+					},
 				}),
 			},
 		},
