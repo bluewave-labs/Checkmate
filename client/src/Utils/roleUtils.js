73f2f29c2aa73fac349ea5c933e6e620a6ee179d
Alex Holliday <ajhollid@gmail.com>
2025-07-22 13:26:01 -0700

    add valid roles

--- client/src/Utils/roleUtils.js
+++ client/src/Utils/roleUtils.js
@@ -5,6 +5,8 @@ export const ROLES = {
 	DEMO: "demo",
 };
 
+export const VALID_ROLES = [ROLES.ADMIN, ROLES.USER, ROLES.DEMO];
+
 export const EDITABLE_ROLES = [
 	{ role: ROLES.ADMIN, _id: ROLES.ADMIN },
 	{ role: ROLES.USER, _id: ROLES.USER },
