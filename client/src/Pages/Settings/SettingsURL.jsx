1a382229748ee0efdc608f271f57693a941f3167
imjainish <jainishprajapati982@gmail.com>
2025-08-21 22:39:41 -0400

    Fix Disabled Field in Settings

--- client/src/Pages/Settings/SettingsURL.jsx
+++ client/src/Pages/Settings/SettingsURL.jsx
@@ -29,7 +29,7 @@ const SettingsURL = ({ HEADING_SX, handleChange, showURL = false }) => {
 				<Select
 					name="showURL"
 					label={t("settingsPage.urlSettings.label")}
-					value={showURL || ""}
+					value={showURL == true ? true : false} 
 					onChange={handleChange}
 					items={[
 						{ _id: true, name: t("settingsPage.urlSettings.selectEnabled") },
