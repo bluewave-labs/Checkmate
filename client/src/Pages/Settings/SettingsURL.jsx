f43210e29e8521a8c36dd3231ae5128e36cc4d2e
imjainish <jainishprajapati982@gmail.com>
2025-08-21 23:13:50 -0400

    Address PR Comments - Format

--- client/src/Pages/Settings/SettingsURL.jsx
+++ client/src/Pages/Settings/SettingsURL.jsx
@@ -29,7 +29,7 @@ const SettingsURL = ({ HEADING_SX, handleChange, showURL = false }) => {
 				<Select
 					name="showURL"
 					label={t("settingsPage.urlSettings.label")}
-					value={showURL === true} 
+					value={showURL === true}
 					onChange={handleChange}
 					items={[
 						{ _id: true, name: t("settingsPage.urlSettings.selectEnabled") },
