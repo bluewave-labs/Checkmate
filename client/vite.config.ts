3f581ae1b02d85ef0d6641fa54893e4425a6bfa0
Alex Holliday <ajhollid@gmail.com>
2025-09-26 14:32:16 -0700

    esm safe path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({}) => {
 	let version = "3.2.0";
