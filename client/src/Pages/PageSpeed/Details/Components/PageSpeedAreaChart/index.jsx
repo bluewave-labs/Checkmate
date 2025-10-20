4f19e81d347081b089a3ee0118de151da9f5729f
mospell <mospell@github.com>
2025-10-20 20:37:19 +0200

    whoops, missed one

--- client/src/Pages/PageSpeed/Details/Components/PageSpeedAreaChart/index.jsx
+++ client/src/Pages/PageSpeed/Details/Components/PageSpeedAreaChart/index.jsx
@@ -50,7 +50,6 @@ const processDataWithGaps = (data, interval) => {
 };
 
 const createPagination = (data, timeRange) => {
-	console.log(timeRange)
 	if (data.length === 0) return [];
 	const paginatedData = []
 	let currentPage = []
@@ -66,7 +65,7 @@ const createPagination = (data, timeRange) => {
 			startTime = entryTime
 		}
 	})
-	
+
 	if (currentPage.length > 0){
 		paginatedData.push(currentPage)
 	}
