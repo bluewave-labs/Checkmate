a88e75841d9b667966b708c884bd6ef4678f13d2
m25chouk <mouad.chouki@imt-atlantique.net>
2025-12-06 19:05:21 +0100

    feat: correct incident summary endpoint #2792

--- server/src/config/routes.js
+++ server/src/config/routes.js
@@ -12,7 +12,7 @@ import QueueRoutes from "../routes/v1/queueRoute.js";
 import LogRoutes from "../routes/v1/logRoutes.js";
 import DiagnosticRoutes from "../routes/v1//diagnosticRoute.js";
 import NotificationRoutes from "../routes/v1/notificationRoute.js";
-import SummaryRoutes from "../routes/v1/summaryRoute.js";
+import SummaryRoutes from "../routes/v1/summaryRoute.js"; //for Homepage widget
 
 
 import IncidentRoutes from "../routes/v1/incidentRoute.js";
