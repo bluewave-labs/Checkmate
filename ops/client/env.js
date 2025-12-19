d139e0b643922fb77266fa9393fae365bacbeb15
Alex Holliday <ajhollid@gmail.com>
2025-12-19 22:28:08 +0000

    cleanup

---
+++ ops/client/env.js
@@ -0,0 +1,6 @@
+window.__ENV = {
+  GLOBAL_REFRESH: "60000",
+  APP_HOST: "https://checkmate-ng.checkmate.so",
+  APP_API_BASE_URL: "https://checkmate-ng.checkmate.so/api/v1",
+  APP_DEPLOYMENT_MODE: "self_hosted",
+};
