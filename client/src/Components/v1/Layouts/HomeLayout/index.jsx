9d44e11496f7851db2bcdea1ec54a395c86f4560
gorkem-bwl <gorkem.cetin@bluewavelabs.ca>
2026-01-17 14:22:07 -0500

    refactor: migrate HomeLayout from CSS to MUI sx styling

--- client/src/Components/v1/Layouts/HomeLayout/index.jsx
+++ client/src/Components/v1/Layouts/HomeLayout/index.jsx
@@ -1,19 +1,40 @@
 import Sidebar from "../../Sidebar/index.jsx";
 import { Outlet } from "react-router";
 import { Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import { UserGuideSidebar } from "../../UserGuide";
 
-import "./index.css";
-
 const HomeLayout = () => {
+	const theme = useTheme();
+
 	return (
 		<Stack
-			className="home-layout"
 			flexDirection="row"
 			gap={14}
+			sx={{
+				position: "relative",
+				minHeight: "100vh",
+				margin: "0 auto",
+				padding: 0,
+				overflowX: "hidden",
+				width: "100%",
+				"& > div": {
+					minHeight: `calc(100vh - ${theme.spacing(4)} * 2)`,
+					flex: 1,
+				},
+			}}
 		>
 			<Sidebar />
-			<Stack className="home-content-wrapper">
+			<Stack
+				sx={{
+					padding: theme.spacing(4),
+					maxWidth: 1400,
+					margin: "0 auto",
+					flex: 1,
+					minWidth: 0,
+					overflowX: "hidden",
+				}}
+			>
 				<Outlet />
 			</Stack>
 			<UserGuideSidebar />
