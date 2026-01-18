3e8d4cb49bb47123ee4390feec240cc12863d26e
Alex Holliday <ajhollid@gmail.com>
2026-01-18 05:38:50 +0000

    convert to ts

--- server/src/repositories/invites/IInvitesRepository.ts
+++ server/src/repositories/invites/IInvitesRepository.ts
@@ -2,7 +2,9 @@ import type { Invite } from "@/types/index.js";
 
 export interface IInvitesRepository {
 	// create
+	create(invite: Partial<Invite>): Promise<Invite>;
 	// fetch
+	findByToken(token: string): Promise<Invite>;
 	findByTokenAndDelete(token: string): Promise<Invite>;
 	// update
 
