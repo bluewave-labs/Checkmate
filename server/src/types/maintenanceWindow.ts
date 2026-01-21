82cc289f53167a59aee9f2a9744002b109f17231
Alex Holliday <ajhollid@gmail.com>
2026-01-21 14:26:17 -0800

    mw type

--- server/src/types/maintenanceWindow.ts
+++ server/src/types/maintenanceWindow.ts
@@ -2,44 +2,10 @@ export interface MaintenanceWindow {
 	monitorId: String;
 	teamId: string;
 	active: boolean;
+	name: string;
+	repeat: number;
+	start: string;
+	end: string;
+	createdAt: string;
+	updatedAt: string;
 }
-const MaintenanceWindow = mongoose.Schema(
-	{
-		monitorId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Monitor",
-			immutable: true,
-		},
-		teamId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Team",
-			immutable: true,
-		},
-		active: {
-			type: Boolean,
-			default: true,
-		},
-		name: {
-			type: String,
-		},
-		repeat: {
-			type: Number,
-		},
-		start: {
-			type: Date,
-		},
-		end: {
-			type: Dat,
-		},
-		expiry: {
-			type: Date,
-			index: { expires: "0s" },
-		},
-	},
-
-	{
-		timestamps: true,
-	}
-);
-
-export default mongoose.model("MaintenanceWindow", MaintenanceWindow);
