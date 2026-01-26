bbdfc00eb9e874ddf04e4a821ca7c1b3c1c10582
Alex Holliday <ajhollid@gmail.com>
2026-01-26 23:45:28 +0000

    format

--- client/src/Components/v2/monitors/charts/HistogramPageSpeedTooltip.tsx
+++ client/src/Components/v2/monitors/charts/HistogramPageSpeedTooltip.tsx
@@ -33,9 +33,7 @@ export const HistogramPageSpeedTooltip = ({
 			<Typography>
 				{formatDateWithTz(data?.date, "ddd, MMMM D, YYYY, HH:mm A", uiTimezone)}
 			</Typography>
-			<Typography>
-				Score: {data?.score}
-			</Typography>
+			<Typography>Score: {data?.score}</Typography>
 		</Stack>
 	);
 };
