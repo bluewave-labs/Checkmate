63e42b727638ed8734ef9dba1ae90f12fc1ca570
Br0wnHammer <kumararyaman26@gmail.com>
2026-02-03 22:36:19 +0530

    Fix: Send Email Invite

--- client/src/Components/v2/monitors/charts/HistogramPageSpeedDetailsTooltip.tsx
+++ client/src/Components/v2/monitors/charts/HistogramPageSpeedDetailsTooltip.tsx
@@ -8,9 +8,8 @@ import type { TooltipProps } from "recharts";
 import { useSelector } from "react-redux";
 import type { RootState } from "@/Types/state";
 
-interface HistogramPageSpeedScoresTooltipProps extends Partial<
-	TooltipProps<number, string>
-> {
+interface HistogramPageSpeedScoresTooltipProps
+	extends Partial<TooltipProps<number, string>> {
 	config: Record<string, ConfigItem>;
 }
 export const HistogramPageSpeedScoresTooltip = ({
--- server/src/controllers/inviteController.ts
+++ server/src/controllers/inviteController.ts
@@ -37,7 +37,7 @@ class InviteController {
 			await inviteBodyValidation.validateAsync(inviteRequest);
 
 			const inviteToken = await this.inviteService.sendInviteEmail({
-				inviteRequest,
+				invite: inviteRequest,
 				firstName: req?.user?.firstName,
 			});
 			return res.status(200).json({
