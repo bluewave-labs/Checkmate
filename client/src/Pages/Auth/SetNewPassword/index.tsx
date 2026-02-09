4db494722089b1ebb0ad3159320a18ba07149ed8
Alex Holliday <ajhollid@gmail.com>
2026-02-09 21:21:17 +0000

    basic UI

--- client/src/Pages/Auth/SetNewPassword/index.tsx
+++ client/src/Pages/Auth/SetNewPassword/index.tsx
@@ -1,12 +1,37 @@
-import { BaseAuthPage } from "@/Components/v2/design-elements";
+import { BaseAuthPage, TextLink } from "@/Components/v2/design-elements";
+import { Button, TextField } from "@/Components/v2/inputs";
+import { useTranslation } from "react-i18next";
 
 const SetNewPasswordPage = () => {
+	const { t } = useTranslation();
 	return (
 		<BaseAuthPage
-			title="setNewPassword"
-			subtitle="new password"
+			title={t("pages.auth.setNewPassword.title")}
+			subtitle={t("pages.auth.setNewPassword.subtitle")}
 		>
-			Test
+			<TextField
+				type="password"
+				fieldLabel={t("pages.auth.common.form.option.password.label")}
+				placeholder={t("pages.auth.common.form.option.password.placeholder")}
+			/>
+			<TextField
+				type="password"
+				fieldLabel={t("pages.auth.common.form.option.confirmPassword.label")}
+				placeholder={t("pages.auth.common.form.option.password.placeholder")}
+			/>
+			<Button
+				variant="contained"
+				type="submit"
+				fullWidth
+			>
+				{t("auth.forgotPassword.buttons.resetPassword")}
+			</Button>
+			<TextLink
+				alignSelf="center"
+				text={t("pages.auth.forgotPassword.links.login.text")}
+				linkText={t("pages.auth.forgotPassword.links.login.linkText")}
+				href="/login"
+			/>
 		</BaseAuthPage>
 	);
 };
