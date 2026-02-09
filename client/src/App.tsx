a20fdb423a8158d4ce4c13d7631e1bbe1bcc8bff
Alex Holliday <ajhollid@gmail.com>
2026-02-09 23:51:18 +0000

    remove unused

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, type CSSProperties } from "react";
 import { useSelector } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
@@ -9,9 +9,10 @@ import { CssBaseline, GlobalStyles } from "@mui/material";
 import { logger } from "./Utils/Logger"; // Import the logger
 import { Routes } from "./Routes";
 import AppLayout from "@/Components/v1/Layouts/AppLayout";
+import type { RootState } from "@/Types/state";
 
 function App() {
-	const mode = useSelector((state) => state.ui.mode);
+	const mode = useSelector((state: RootState) => state.ui.mode);
 
 	// Cleanup
 	useEffect(() => {
@@ -28,6 +29,7 @@ function App() {
 			<GlobalStyles
 				styles={{
 					body: {
+						// @ts-ignore
 						backgroundColor: theme.palette.background.main,
 					},
 				}}
@@ -38,10 +40,12 @@ function App() {
 			<ToastContainer
 				newestOnTop={true}
 				theme={mode}
-				style={{
-					"--toastify-color-progress-light": "#7C8BA1",
-					"--toastify-color-progress-dark": "#7C8BA1",
-				}}
+				style={
+					{
+						"--toastify-color-progress-light": "#7C8BA1",
+						"--toastify-color-progress-dark": "#7C8BA1",
+					} as CSSProperties
+				}
 			/>
 		</ThemeProvider>
 	);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { persistor, store } from "./store";
+import { persistor, store } from "./store.js";
 import { PersistGate } from "redux-persist/integration/react";
-import I18nLoader from "./Components/v1/I18nLoader";
-import { initApiClient } from "./Utils/ApiClient";
+import I18nLoader from "./Components/v1/I18nLoader/index.jsx";
+import { initApiClient } from "./Utils/ApiClient.js";
 
 initApiClient(store);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
 	<Provider store={store}>
 		<PersistGate
 			loading={null}
