f5372b4d869c54171d8a0a1edddf9960f117fc14
Alex Holliday <ajhollid@gmail.com>
2026-02-09 21:29:55 +0000

    ui and strings

--- client/src/Pages/Auth/SetNewPassword/index.tsx
+++ client/src/Pages/Auth/SetNewPassword/index.tsx
@@ -1,9 +1,17 @@
-import { BaseAuthPage, TextLink } from "@/Components/v2/design-elements";
+import {
+	BaseAuthPage,
+	BulletPointCheck,
+	TextLink,
+} from "@/Components/v2/design-elements";
 import { Button, TextField } from "@/Components/v2/inputs";
+import Stack from "@mui/material/Stack";
+import { useTheme } from "@mui/material/styles";
 import { useTranslation } from "react-i18next";
 
 const SetNewPasswordPage = () => {
+	const theme = useTheme();
 	const { t } = useTranslation();
+
 	return (
 		<BaseAuthPage
 			title={t("pages.auth.setNewPassword.title")}
@@ -19,6 +27,32 @@ const SetNewPasswordPage = () => {
 				fieldLabel={t("pages.auth.common.form.option.confirmPassword.label")}
 				placeholder={t("pages.auth.common.form.option.password.placeholder")}
 			/>
+			<Stack gap={theme.spacing(4)}>
+				<BulletPointCheck
+					text={t("auth.common.passwordRules.length")}
+					variant="info"
+				/>
+				<BulletPointCheck
+					text={t("auth.common.passwordRules.special")}
+					variant="info"
+				/>
+				<BulletPointCheck
+					text={t("auth.common.passwordRules.number")}
+					variant="info"
+				/>
+				<BulletPointCheck
+					text={t("auth.common.passwordRules.uppercase")}
+					variant="info"
+				/>
+				<BulletPointCheck
+					text={t("auth.common.passwordRules.lowercase")}
+					variant="info"
+				/>
+				<BulletPointCheck
+					text={t("auth.common.passwordRules.match")}
+					variant="info"
+				/>
+			</Stack>
 			<Button
 				variant="contained"
 				type="submit"
