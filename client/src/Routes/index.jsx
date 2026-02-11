64438020243d4671a526af0f7b1ef9d8eb14d4ef
Alex Holliday <ajhollid@gmail.com>
2026-02-11 17:21:32 +0000

    cleanup imports

--- client/src/Routes/index.jsx
+++ client/src/Routes/index.jsx
@@ -11,7 +11,7 @@ import NotFound from "@/Pages/NotFound";
 import AuthLogin from "@/Pages/Auth/Login";
 import AuthRegister from "@/Pages/Auth/Register";
 import AuthForgotPassword from "@/Pages/Auth/Recovery";
-import AuthSetNewPassword from "../Pages/Auth/SetNewPassword";
+import AuthSetNewPassword from "@/Pages/Auth/SetNewPassword";
 
 // Uptime
 import Uptime from "@/Pages/Uptime/Monitors";
@@ -23,34 +23,34 @@ import PageSpeedDetails from "@/Pages/PageSpeed/Details/";
 
 // Infrastructure
 import Infrastructure from "@/Pages/Infrastructure/Monitors";
-import InfrastructureDetails from "@/Pages/Infrastructure/Details/index";
+import InfrastructureDetails from "@/Pages/Infrastructure/Details";
 
 // Checks
-import Checks from "../Pages/Checks/index";
+import Checks from "@/Pages/Checks";
 
 // Incidents
-import Incidents from "../Pages/Incidents/";
+import Incidents from "@/Pages/Incidents";
 
 // Status pages
-import CreateStatus from "../Pages/StatusPage/Create/";
-import StatusPages from "../Pages/StatusPage/StatusPages";
-import Status from "../Pages/StatusPage/Status";
+import CreateStatus from "@/Pages/StatusPage/Create/";
+import StatusPages from "@/Pages/StatusPage/StatusPages";
+import Status from "@/Pages/StatusPage/Status";
 
-import Notifications from "../Pages/Notifications";
-import CreateNotifications from "../Pages/Notifications/create";
+import Notifications from "@/Pages/Notifications";
+import CreateNotifications from "@/Pages/Notifications/create";
 
 // Settings
 import Account from "@/Pages/Account";
-import EditUser from "../Pages/Account/EditUser";
-import Settings from "../Pages/Settings";
+import EditUser from "@/Pages/Account/EditUser";
+import Settings from "@/Pages/Settings";
 
-import Maintenance from "../Pages/Maintenance";
+import Maintenance from "@/Pages/Maintenance";
 import CreateNewMaintenanceWindow from "@/Pages/Maintenance/create";
 
-import ProtectedRoute from "../Components/v1/ProtectedRoute";
-import RoleProtectedRoute from "../Components/v1/RoleProtectedRoute";
+import ProtectedRoute from "@/Components/v1/ProtectedRoute";
+import RoleProtectedRoute from "@/Components/v1/RoleProtectedRoute";
 import withAdminCheck from "@/Components/v1/HOC/withAdminCheck";
-import Logs from "../Pages/Logs";
+import Logs from "@/Pages/Logs";
 
 import CreateMonitor from "@/Pages/CreateMonitor";
 
