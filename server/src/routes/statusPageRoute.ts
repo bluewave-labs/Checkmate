dae2709d317632d65da290cb33b2ce2818df2c13
Br0wnHammer <kumararyaman26@gmail.com>
2026-02-19 23:55:43 +0530

    Fix: Authentication Middleware in StatusPage

--- server/src/routes/statusPageRoute.ts
+++ server/src/routes/statusPageRoute.ts
@@ -18,7 +18,7 @@ class StatusPageRoutes {
 		this.router.post("/", upload.single("logo"), verifyJWT, this.statusPageController.createStatusPage);
 		this.router.put("/:id", upload.single("logo"), verifyJWT, this.statusPageController.updateStatusPage);
 
-		this.router.get("/:url", this.statusPageController.getStatusPageByUrl);
+		this.router.get("/:url", verifyJWT, this.statusPageController.getStatusPageByUrl);
 		this.router.delete("/:id", verifyJWT, this.statusPageController.deleteStatusPage);
 	}
 
