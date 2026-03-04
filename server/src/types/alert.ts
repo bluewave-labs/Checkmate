d665e95f024881a9593424c3ebefa70fdeabf440
Alex Holliday <ajhollid@gmail.com>
2026-03-04 17:39:31 +0000

    discord

--- server/src/types/alert.ts
+++ server/src/types/alert.ts
@@ -15,6 +15,20 @@ export interface AlertMatrixPayload {
 	htmlText: string;
 }
 
+export interface DiscordEmbedField {
+	name: string;
+	value: string;
+	inline?: boolean;
+}
+
+export interface AlertDiscordPayload {
+	title: string;
+	description: string;
+	color: number;
+	fields: DiscordEmbedField[];
+	timestamp: string;
+}
+
 export interface Alert {
 	channel: NotificationChannel;
 	address?: string;
