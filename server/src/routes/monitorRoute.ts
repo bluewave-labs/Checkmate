d4a8023a76467981a353f69cbc79f190d2427a82
Alex Holliday <ajhollid@gmail.com>
2026-03-20 11:47:22 -0700

    monitors

--- server/src/routes/monitorRoute.ts
+++ server/src/routes/monitorRoute.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
 import { isAllowed } from "@/middleware/isAllowed.js";
-import multer from "multer";
-import { fetchMonitorCertificate } from "@/controllers/controllerUtils.js";
-const upload = multer({
-	storage: multer.memoryStorage(), // Store file in memory as Buffer
-});
+import { IMonitorController } from "@/controllers/monitorController.js";
 
 class MonitorRoutes {
 	private router: Router;
-	private monitorController: any;
-	constructor(monitorController: any) {
+	private monitorController: IMonitorController;
+	constructor(monitorController: IMonitorController) {
 		this.router = Router();
 		this.monitorController = monitorController;
 		this.initRoutes();
@@ -38,7 +34,7 @@ class MonitorRoutes {
 
 		// Util routes
 		this.router.get("/certificate/:monitorId", (req, res, next) => {
-			this.monitorController.getMonitorCertificate(req, res, next, fetchMonitorCertificate);
+			this.monitorController.getMonitorCertificate(req, res, next);
 		});
 
 		// General monitor CRUD routes
