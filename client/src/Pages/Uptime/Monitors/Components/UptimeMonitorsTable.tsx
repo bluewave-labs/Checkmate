744d8c8dcf541f93affc2397e2148bd10e900e97
ashu130698 <ashushukla299@gmail.com>
2026-03-31 00:20:21 +0530

    style: fix code format issues

--- client/src/Pages/Uptime/Monitors/Components/UptimeMonitorsTable.tsx
+++ client/src/Pages/Uptime/Monitors/Components/UptimeMonitorsTable.tsx
@@ -292,7 +292,10 @@ export const MonitorTable = ({
 	return (
 		<Box>
 			{isSmall && (
-				<Box px={1} pb={2}>
+				<Box
+					px={1}
+					pb={2}
+				>
 					<Stack
 						direction="row"
 						alignItems="center"
