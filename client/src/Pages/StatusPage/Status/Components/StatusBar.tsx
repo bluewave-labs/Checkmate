7b164e4df536965623a6a007166712378762dd54
SkyKingPX <skyking_px@soncresity.dev>
2026-04-16 13:37:54 +0200

    Make requested changes

--- client/src/Pages/StatusPage/Status/Components/StatusBar.tsx
+++ client/src/Pages/StatusPage/Status/Components/StatusBar.tsx
@@ -1,7 +1,7 @@
 import {
 	AlertTriangle,
 	CircleCheck,
-	CircleX,
+	CircleX, Loader,
 	PauseCircle,
 	ShieldAlert,
 	Wrench,
@@ -14,10 +14,24 @@ import { useTheme } from "@mui/material";
 import type { Theme } from "@mui/material";
 import type { Monitor, MonitorStatus } from "@/Types/Monitor";
 
+interface StatusDisplay {
+	icon: JSX.Element;
+	msg?: string;
+	color?: string;
+}
+
 const getMonitorStatus = (monitors: Monitor[], theme: Theme, t: Function) => {
-	const monitorsStatus: Record<string, any> = {
+	const monitorsStatus: StatusDisplay = {
 		icon: <AlertTriangle size={24} />,
 	};
+
+	// Handle empty monitors array
+	if (monitors.length === 0) {
+		monitorsStatus.msg = t("pages.statusPages.statusBar.noMonitors");
+		monitorsStatus.color = theme.palette.warning.main;
+		monitorsStatus.icon = <CircleX size={24} />;
+		return monitorsStatus;
+	}
 
 	const allOf = (...statuses: MonitorStatus[]) =>
 		monitors.every((m) => statuses.includes(m.status));
@@ -33,7 +47,7 @@ const getMonitorStatus = (monitors: Monitor[], theme: Theme, t: Function) => {
 		monitorsStatus.icon = <CircleCheck size={24} />;
 	} else if (allOf("breached")) {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.allBreached");
-		monitorsStatus.color = theme.palette.error.dark;
+		monitorsStatus.color = theme.palette.error.main;
 		monitorsStatus.icon = <ShieldAlert size={24} />;
 	} else if (allOf("maintenance")) {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.allMaintenance");
@@ -47,15 +61,19 @@ const getMonitorStatus = (monitors: Monitor[], theme: Theme, t: Function) => {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.allPaused");
 		monitorsStatus.color = theme.palette.warning.main;
 		monitorsStatus.icon = <PauseCircle size={24} />;
+	} else if (allOf("initializing")) {
+		monitorsStatus.msg = t("pages.statusPages.statusBar.allInitializing");
+		monitorsStatus.color = theme.palette.warning.main;
+		monitorsStatus.icon = <Loader size={24} />;
 
 		// Breached takes highest priority in mixed states
 	} else if (someOf("breached") && someOf("down")) {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.breachedAndDown");
-		monitorsStatus.color = theme.palette.error.dark;
+		monitorsStatus.color = theme.palette.error.main;
 		monitorsStatus.icon = <ShieldAlert size={24} />;
 	} else if (someOf("breached")) {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.breached");
-		monitorsStatus.color = theme.palette.error.dark;
+		monitorsStatus.color = theme.palette.error.main;
 		monitorsStatus.icon = <ShieldAlert size={24} />;
 
 		// Maintenance combinations
@@ -63,23 +81,28 @@ const getMonitorStatus = (monitors: Monitor[], theme: Theme, t: Function) => {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.maintenanceAndDown");
 		monitorsStatus.color = theme.palette.error.main;
 		monitorsStatus.icon = <Wrench size={24} />;
-	} else if (someOf("maintenance") && noneOf("down", "breached")) {
+	} else if (someOf("maintenance") && noneOf("down")) {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.maintenance");
 		monitorsStatus.color = theme.palette.warning.main;
 		monitorsStatus.icon = <Wrench size={24} />;
 
 		// Degraded (some down, no maintenance/breached)
-	} else if (someOf("down") && noneOf("maintenance", "breached")) {
+	} else if (someOf("down")) {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.degraded");
 		monitorsStatus.color = theme.palette.warning.main;
 		monitorsStatus.icon = <AlertTriangle size={24} />;
 
 		// Some Paused
-	} else if (someOf("paused") && noneOf("down", "breached", "maintenance")) {
+	} else if (someOf("paused")) {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.partiallyPaused");
 		monitorsStatus.color = theme.palette.warning.main;
 		monitorsStatus.icon = <PauseCircle size={24} />;
 
+		// Initializing
+	} else if (someOf("initializing")) {
+		monitorsStatus.msg = t("pages.statusPages.statusBar.initializing");
+		monitorsStatus.color = theme.palette.info.main;
+		monitorsStatus.icon = <Loader size={24} />;
 	} else {
 		monitorsStatus.msg = t("pages.statusPages.statusBar.unknown");
 		monitorsStatus.color = theme.palette.warning.main;
